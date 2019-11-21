f4594c366dcf2deaad5186f1c4d4027552fa28f8
Karl Stolley <karl.stolley@gmail.com>
2019-11-21 15:03:55 -0600

    Set the foundations for building a scaffold of directories

--- lib/jtoc.rb
+++ lib/jtoc.rb
@@ -3,11 +3,31 @@ module JTOpenCourse
   VERSION = "0.0.4-alpha"
 
   class SiteBuilder
-    class << self
-      def doit
-        puts "Okay, let's do it! Simply!"
+    SCAFFOLD_DIRECTORIES = %w(
+      _data policies projects syllabus/_policies syllabus/_projects syllabus/_weeks
+    ).freeze
+
+    attr_reader :name, :path
+
+    def initialize(course_name)
+      @name = course_name.chomp
+      @path = Pathname.new(File.expand_path(name, Dir.pwd))
+    end
+
+    def create!
+      create_directories
+    end
+
+    def create_directories
+      mkdir_p(SCAFFOLD_DIRECTORIES)
+    end
+
+    def mkdir_p(directories)
+      Array(directories).each do |d|
+        FileUtils.mkdir_p(path.join(d))
       end
     end
+
   end
 
 end
