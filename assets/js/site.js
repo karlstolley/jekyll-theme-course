703684612c057c668f584fb1e6588dfed70c0698
Karl Stolley <karl.stolley@gmail.com>
2020-01-20 13:44:27 -0600

    Show 'This Week' on the current week

--- assets/js/site.js
+++ assets/js/site.js
@@ -4,6 +4,7 @@ if (document.querySelector('#calendar')) {
   var current_week = this_week.cloneNode(true); // make a copy of it,
   this_week.querySelector('#this-week').id = ''; // remove the original #this-week id
   current_week.classList.add('current'); // add a class of current to this week's article copy
+  current_week.querySelector('#this-week small').innerText = "This Week";
   document.querySelector('#content').prepend(current_week); // insert the copy at the top of the calendar
   if (location.hash) {
     location.hash = '#this-week'; // point at the new hash position; viewport should show this one
