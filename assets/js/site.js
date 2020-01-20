af7ede629d17be9b69983685cc9f9dbbfc53364d
Karl Stolley <karl.stolley@gmail.com>
2020-01-20 13:32:48 -0600

    Duplicate and highlight the current week

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,3 +1,13 @@
+// Capture and replicate the current week at the top of the calendar
+if (document.querySelector('#calendar')) {
+  var this_week = document.querySelector('#this-week').closest('article'); // grab this week's <article>
+  var current_week = this_week.cloneNode(true); // make a copy of it,
+  this_week.querySelector('#this-week').id = ''; // remove the original #this-week id
+  current_week.classList.add('current'); // add a class of current to this week's article copy
+  document.querySelector('#content').prepend(current_week); // insert the copy at the top of the calendar
+  location.hash = '#this-week'; // point at the new hash position; viewport should show this one
+}
+
 window.addEventListener('keyup', function(e) {
   console.log(e.keyCode);
   // Toggle the visibility of gridlines when `g` is pressed
