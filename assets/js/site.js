df4a1a57c9a29fd4df53d9a0a01a6bacda29ea37
Karl Stolley <karl.stolley@gmail.com>
2020-02-06 13:26:51 -0600

    Stop logging keyCode to the console

--- assets/js/site.js
+++ assets/js/site.js
@@ -15,7 +15,7 @@ if (document.querySelector('#calendar')) {
 }
 
 window.addEventListener('keyup', function(e) {
-  console.log(e.keyCode);
+  // console.log(e.keyCode);
   // Toggle the visibility of gridlines when `g` is pressed
   if (e.keyCode === 71) {
     document.querySelector('html').classList.toggle('g');
