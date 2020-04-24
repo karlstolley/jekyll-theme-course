7e48c4824a1b2527f4f93cf85b47f8e49afc0343
Karl Stolley <karl.stolley@gmail.com>
2020-04-24 13:04:01 -0500

    Read and inject latest commit from GitHub

--- assets/js/site.js
+++ assets/js/site.js
@@ -53,6 +53,49 @@ if (document.querySelector('#calendar')) {
   current_week.insertAdjacentElement('afterend', btn_show_calendar);
 }
 
+if ('fetch' in window) {
+  var namedDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  var namedMonths = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
+
+  var github_url = (function() {
+    var url = document.querySelector('#github').getAttribute('href'); // grab the href value of the repo link
+    if (typeof(url) !== 'undefined') {
+      var fragment = url.substring(url.indexOf('.com/') + 5); // find the tail end (5 = .com/)
+      return 'https://api.github.com/repos/' + fragment + '/commits?per_page=1'; // return the API url
+    }
+  })();
+
+  function escapeHTML(str) {
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
+
+  if(typeof(github_url) !== "undefined") {
+  fetch(github_url)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    var commit = {};
+    data = data[0]; // only need most recent commit
+    // Lowercase commit message's first word to run in `...to XYZ` copy:
+    commit.message = data.commit.message.charAt(0).toLowerCase() + data.commit.message.slice(1);
+    commit.url = data.html_url;
+    commit.stamp = data.commit.author.date;
+    commit.date = new Date(commit.stamp);
+    // Put the date in Day, Month 31 at <Local Time String> format
+    commit.time_string = namedDays[commit.date.getDay()] + ', ' +
+      namedMonths[commit.date.getMonth()] + ' ' +
+      commit.date.getDate() + ' at ' + commit.date.toLocaleTimeString();
+    // Append to footer on calendar
+    document.querySelector('#footer p').innerHTML +=
+      ' Course last updated on <time datetime="' + commit.stamp + '">' + commit.time_string +
+      '</time> to <a id="commit-message" href="' + commit.url + '">' + escapeHTML(commit.message) + '</a>.';
+  });
+}
+}
+
 window.addEventListener('keyup', function(e) {
   // console.log(e.keyCode);
   // Toggle the visibility of gridlines when `g` is pressed
