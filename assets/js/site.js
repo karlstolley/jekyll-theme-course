0e7c5213354a2c9318a20fb51ce1aa3926771bd1
Karl Stolley <karl.stolley@gmail.com>
2020-04-26 22:50:29 -0500

    Improve theme-toggle icons and style

--- assets/js/site.js
+++ assets/js/site.js
@@ -34,6 +34,11 @@ function themeSwitcher() {
   var header = document.querySelector('#header h1');
   var toggle = document.createElement('a');
   var html = document.querySelector('html');
+  // Icons from https://remixicon.com/
+  var icons = {
+    light: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 18a6 6 0 1 1 0-12 6 6 0 0 1 0 12zm0-2a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM11 1h2v3h-2V1zm0 19h2v3h-2v-3zM3.515 4.929l1.414-1.414L7.05 5.636 5.636 7.05 3.515 4.93zM16.95 18.364l1.414-1.414 2.121 2.121-1.414 1.414-2.121-2.121zm2.121-14.85l1.414 1.415-2.121 2.121-1.414-1.414 2.121-2.121zM5.636 16.95l1.414 1.414-2.121 2.121-1.414-1.414 2.121-2.121zM23 11v2h-3v-2h3zM4 11v2H1v-2h3z"/></svg>',
+    dark: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M11.38 2.019a7.5 7.5 0 1 0 10.6 10.6C21.662 17.854 17.316 22 12.001 22 6.477 22 2 17.523 2 12c0-5.315 4.146-9.661 9.38-9.981z"/></svg>'
+  }
 
   var dark_mode = false;
   var modes = ['light','dark'];
@@ -58,7 +63,7 @@ function themeSwitcher() {
   toggle.id = 'theme-toggle';
   toggle.href = '#null';
   toggle.title = 'Switch to ' + modes[1] + ' theme';
-  toggle.innerText = '☯︎';
+  toggle.innerHTML = icons[modes[1]];
   header.appendChild(toggle);
 
   toggle.addEventListener('click', function(e) {
@@ -69,6 +74,7 @@ function themeSwitcher() {
       localStorage.setItem('modes',modes.join(','));
     }
     toggle.title = 'Switch to ' + modes[1] + ' theme';
+    toggle.innerHTML = icons[modes[1]];
   });
 
   function storageAvailable(type) {
