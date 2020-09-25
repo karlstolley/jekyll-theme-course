6d3268fad64276b85e2023f7346fa72253ad1a99
Karl Stolley <karl.stolley@gmail.com>
2020-09-25 12:38:24 -0500

    Bump version to 1.2.2

--- lib/jtoc.rb
+++ lib/jtoc.rb
@@ -3,7 +3,7 @@ module JTOpenCourse
   require 'erb'
   require 'date'
 
-  VERSION = "1.2.1"
+  VERSION = "1.2.2"
 
   SPELLED_NUMS = %w(
     Zero One Two Three Four Five Six Seven Eight Nine Ten Eleven Twelve Thirteen Fourteen Fifteen
